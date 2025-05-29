d400c7a4da5302a4b983982def4276c4c1f19d4f
MartinMSalas <ingmartinmsalas@gmail.com>
2025-05-29 10:48:38 -0300

    feat<BreweryClient>: externalice properties

--- src/test/java/com/mmstechnology/mssc_brewery_client/web/client/BreweryClientTest.java
+++ src/test/java/com/mmstechnology/mssc_brewery_client/web/client/BreweryClientTest.java
@@ -2,6 +2,7 @@ package com.mmstechnology.mssc_brewery_client.web.client;
 
 import com.mmstechnology.mssc_brewery_client.web.model.BeerRecord;
 import com.mmstechnology.mssc_brewery_client.web.model.BeerStyleEnum;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +18,7 @@ class BreweryClientTest {
     @Autowired
     BreweryClient breweryClient;
 
-    @Test
+    @RepeatedTest(1)
     void getBeerById() {
         BeerRecord beerRecord = breweryClient.getBeerById(UUID.randomUUID());
         assertNotNull(beerRecord);
@@ -40,5 +41,26 @@ class BreweryClientTest {
 
         assertNotNull(savedBeer);
         assertEquals(beerRecord.beerName(), savedBeer.beerName());
+    }
+
+    @Test
+    void testUpdateBeer() {
+        BeerRecord beerRecord = BeerRecord.builder()
+                .beerId(UUID.randomUUID())
+                .beerName("Test Beer")
+                .beerStyle(BeerStyleEnum.IPA)
+                .upc(123456789012L)
+                .price(BigDecimal.valueOf(12.99))
+                .quantityOnHand(100)
+                .build();
+
+        breweryClient.updateBeer(UUID.randomUUID(), beerRecord);
+
+    }
+
+
+    @Test
+    void testDeleteBeer() {
+        breweryClient.deleteBeer(UUID.randomUUID());
     }
 }
